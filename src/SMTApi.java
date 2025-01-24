b3aa830f4ccc8d18958fbc1d8d9bcb7d1fc22f53
DOLPHINISAF <catalin.nedelescu@yahoo.com>
2025-01-24 22:30:36 +0200

    created default methods

--- src/SMTApi.java
+++ src/SMTApi.java
@@ -1,2 +1,29 @@
+import java.net.http.*;
+
 public class SMTApi {
+
+    private String APIKey;
+
+    private WebSocket webSocket;
+
+    private WebSocket.Listener webSocketListener;
+    SMTApi() {
+
+    }
+    public void SetApiKey(String APIKey){
+        this.APIKey = APIKey;
+    }
+    public void  SendUpdate(String nameID, String value, String status){
+
+    }
+    public void  SendUpdate(String nameID, String value){
+
+    }
+    public void SetStatus(String nameID, String status){
+
+    }
+
+
+
+
 }
